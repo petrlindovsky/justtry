50fb35a33b05b004632cba3ecd32eb3a12277678
Petr Lindovsky <105349859+petrlindovsky@users.noreply.github.com>
2022-05-16 15:20:28 +0200

    Update justtry.py

--- justtry.py
+++ justtry.py
@@ -1,19 +1,3 @@
 import streamlit
 streamlit.text('Hi, Petr!')
 
-from annotated_text import annotated_text
-
-annotated_text(
-    "This ",
-    ("is", "verb"),
-    " some ",
-    ("annotated", "adj"),
-    ("text", "noun"),
-    " for those of ",
-    ("you", "pronoun"),
-    " who ",
-    ("like", "verb"),
-    " this sort of ",
-    ("thing", "noun"),
-    "."
-)
